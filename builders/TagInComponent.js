193ec114d4afa70e49129982c35ff349faabd316
DevonTheFloor <devonthefloor@free.fr>
2022-09-21 19:43:37 +0200

    Update TagInComponent use context and const method

--- builders/TagInComponent.js
+++ builders/TagInComponent.js
@@ -1,119 +1,131 @@
-export default class TagInComponent{
-  constructor( tag, idTag, classAtt, mountOn) {
+export class TagInComponent{
+  constructor( tag, idTag, classAtt, mountOn, getContext) {
     this.tag = tag;
     this.idTag = idTag;
     this.classAtt = classAtt;
     this.mountOn = mountOn;
+    this.getContext = getContext;
 
     const markup = document.createElement(this.tag);
-    console.log('Markip :', markup);
-    console.log('type of: ', typeof(markup));
     markup.id = this.idTag;
     //markup.id = 'id'+markup.tagName+this.number;
     markup.setAttribute('class', this.classAtt);
     if (this.tag === "p") {
-      this.crateAttributeForPara(markup, this.idTag)
+      this.crateAttributeForPara(markup, this.idTag, this.getContext);
     } 
     else if(this.tag === "a") {
-      this.createAttributeForLink(markup, this.idTag)
+      this.createAttributeForLink(markup, this.idTag, this.getContext);
     }
     else if (this.tag === "h1") {
-      this.createAttributeForTitle1(markup, this.idTag)
+      this.createAttributeForTitle1(markup, this.idTag, this.getContext);
     }
     else if (this.tag === "h2") {
-      this.createAttributeForTitle2(markup, this.idTag)
+      this.createAttributeForTitle2(markup, this.idTag, this.getContext);
     }
     else if (this.tag === "h3") {
-      this.createAttributeForTitle3(markup, this.idTag)
+      this.createAttributeForTitle3(markup, this.idTag, this.getContext);
     }
     else if (this.tag === "h4") {
-      this.createAttributeForTitle4(markup, this.idTag)
+      this.createAttributeForTitle4(markup, this.idTag, this.getContext);
     }
     else if (this.tag === "h5") {
-      this.createAttributeForTitle5(markup, this.idTag)
+      this.createAttributeForTitle5(markup, this.idTag, this.getContext);
     }
     else if (this.tag === "h6") {
-      this.createAttributeForTitle6(markup, this.idTag)
+      this.createAttributeForTitle6(markup, this.idTag, this.getContext)
     } else if (this.tag === "img") {
-      this.createAttributeForImg(markup, this.idTag);
+      this.createAttributeForImg(markup, this.idTag, this.getContext);
     }
     this.mountOn.appendChild(markup);
 
+    return markup;
   }
     
-  createAttributeForTitle1(markup, idTag) {
-    const titre = this.mountOn.getAttribute('titre1-'+idTag);
+  createAttributeForTitle1(markup, idTag, context) {
+    const titre = context.getAttribute('titre1-'+idTag);
     markup.textContent = titre;
   }
-  createAttributeForTitle2(markup, idTag) {
-    const titre = this.mountOn.getAttribute('titre2-'+idTag);
+  createAttributeForTitle2(markup, idTag, context) {
+    const titre = context.getAttribute('titre2-'+idTag);
     markup.textContent = titre;
   }
-  createAttributeForTitle3(markup, idTag) {
-    const titre = this.mountOn.getAttribute('titre3-'+idTag);
+  createAttributeForTitle3(markup, idTag, context) {
+    const titre = context.getAttribute('titre3-'+idTag);
     markup.textContent = titre;
   }
-  createAttributeForTitle4(markup, idTag) {
-    const titre = this.mountOn.getAttribute('titre4-'+idTag);
+  createAttributeForTitle4(markup, idTag, context) {
+    const titre = context.getAttribute('titre4-'+idTag);
     markup.textContent = titre;
   }
-  createAttributeForTitle5(markup, idTag) {
-    const titre = this.mountOn.getAttribute('titre5-'+idTag);
+  createAttributeForTitle5(markup, idTag, context) {
+    const titre = context.getAttribute('titre5-'+idTag);
     markup.textContent = titre;
   }
-  createAttributeForTitle6(markup, idTag) {
-    const titre = this.mountOn.getAttribute('titre6-'+idTag);
+  createAttributeForTitle6(markup, idTag, context) {
+    const titre = context.getAttribute('titre6-'+idTag);
     markup.textContent = titre;
   }
-  crateAttributeForPara(markup, idTag) {
-    const content = this.mountOn.getAttribute('texte-'+idTag);
+  crateAttributeForPara(markup, idTag, context) {
+    const content = context.getAttribute('texte-'+idTag);
     markup.textContent = content;
   }
-  createAttributeForLink(markup, idTag) {
-    const site = this.mountOn.getAttribute('site-'+idTag),
-      url = this.mountOn.getAttribute('url-'+idTag),
-      choice = this.mountOn.getAttribute('choice-'+idTag)
+  createAttributeForLink(markup, idTag, context) {
+    const site = context.getAttribute('site-'+idTag),
+      url = context.getAttribute('url-'+idTag),
+      choice = context.getAttribute('choice-'+idTag)
     markup.textContent = site;
     markup.setAttribute('href', url);
     markup.setAttribute('target', choice);
   }
-  createAttributeForImg(markup, idTag) {
-    const source = this.mountOn.getAttribute('source-'+idTag),
-      description = this.mountOn.getAttribute('description-'+idTag);
+  createAttributeForImg(markup, idTag, context) {
+    const source = context.getAttribute('source-'+idTag),
+      description = context.getAttribute('description-'+idTag);
     markup.setAttribute('src', source);
     markup.setAttribute('alt', description);
   }
-  /*liACreatefn(olIdMount, site, url) {
+}
+
+export const tagMethod = {
+  liMenuCreator: (direction, links, olIdMount, site, url) =>{
+    if (direction === "in") {
+      const url = new URL(window.location.href),
+        hach = url.hash;
+      const newLinks = links.filter(link => link.url != hach);
+      newLinks.forEach(link => {
+        linkedListIn(olIdMount, link)
+      })
+    } else {
+      links.forEach(link => {
+        linkedListOut(olIdMount, link);
+      })
+    }
+  }
+}
+  function linkedListIn(olIdMount, link) {
     const li = document.createElement('li'),
-      olMount = document.getElementById(olIdMount);
-    olMount.appendChild(li);
-    //<a></a>1
-    const a = document.createElement('a')
-    a.setAttribute('href', url);
-    a.setAttribute('target', '_blank');
-    a.textContent = site;
-    li.appendChild(a);
-  }*/
-  liMenuCreator(direction, links, olIdMount, site, url) {
-    links.forEach(link => {
-      const li = document.createElement('li'),
       olMount = document.getElementById(olIdMount);
       olMount.appendChild(li);
       //<a></a>1
       const a = document.createElement('a')
       a.setAttribute('href', link.url);
-      if (direction === "out") {
-        a.setAttribute('target', '_blank');
-      }
       a.textContent = link.site;
-      li.appendChild(a);
-    })
+    li.appendChild(a);
   }
-
+  function linkedListOut(olIdMount, link) {
+    const li = document.createElement('li'),
+    olMount = document.getElementById(olIdMount);
+    olMount.appendChild(li);
+    //<a></a>1
+    const a = document.createElement('a')
+    a.setAttribute('href', link.url);
+    a.setAttribute('target', '_blank');
+    a.textContent = link.site;
+    li.appendChild(a);
+  }
+  
   /*const links = [
       { site: "", url: ""},
       { site: "", url: "" },
       { site: "", url: "" },
       { site: "", url: "" }
     ]*/
-}
