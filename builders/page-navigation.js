e3cda06e77ca8aedefad91d4cca61ff987ab3212
DevonTheFloor <devonthefloor@free.fr>
2022-11-04 17:22:31 +0100

    MAJ navigator

--- builders/page-navigation.js
+++ builders/page-navigation.js
@@ -4,59 +4,32 @@ import { dQSr } from '../helpers/myDomHelper.js';
 
 export function navigator() {
   const url = new URL(window.location.href),
-    fullHach = url.hash,
     path = url.pathname,
-    splitor = fullHach.split('/'),
-    recomp = `${splitor[0]}/${splitor[1]}`,
-    longueur = splitor.length;
-    if (longueur <= 2 ) {
-      let pat = 'premier if';
-      mapForHashUrl(fullHach);
-    } else if (longueur <= 3 && splitor[2] === ""){
-      mapForHashUrl(recomp);
+    pa = mapping.find(p => p.uri === path),
+    customMetas = dQSAl('meta.custom-meta');
+    for(let meta of customMetas) {
+      meta.remove();
+      console.log(customMetas.length);
+    }
+    if (pa === undefined ) {
+      setTimeout(()=> {
+        window.location.assign('/');
+      }, 3000)
+      dQSr('#app').innerHTML = `
+            <h1>ERROR 404</h1>
+            <p>La page que vous cherchez n'existe pas</p>
+          `;
+          window.scroll(0, 0);
     } else {
-      mapForHashUrl(recomp);
+      insertPageInApp(pa.page);
+      window.scroll(0,0);
     }
 }
 
-function mapForHashUrl(url) {
-  let pa = mapping.find(p => p.uri === url);
-  console.log('PA :', pa);
-  if (pa === undefined ) {
-    setTimeout(()=> {
-      window.location.assign('#/');
-    }, 3000)
-    dQSr('#app').innerHTML = `
-          <h1>ERROR 404</h1>
-          <p>La page que vous cherchez n'existe pas</p>
-        `;
-        window.scroll(0, 0);
-  } else {
-    insertPageInApp(pa.page);
-
-  }
-
-}
 export function activatedNavigator() {
   setTimeout(()=>{
     navigator();
   },10)
-}
-
-export function listenForHash() {
-  const url = new URL(window.location.href),
-    hach = url.hash;
-  if(!hach) {
-    window.location.assign("#/");
-    activatedNavigator();
-  } else {
-    activatedNavigator()
-  }
-}
-export function listenForHashInIndex() {
-  window.addEventListener('hashchange',()=>{
-    activatedNavigator()
-  })  
 }
 
 export function hashNavigatorSearchParams(...paramsName) {
