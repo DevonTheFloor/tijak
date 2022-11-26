10f1e83cfa8ae16baea1753b242a9ccf943eb5cc
DevonTheFloor <devonthefloor@free.fr>
2022-11-26 19:20:01 +0100

    UpDate myFectg Helpers

--- helpers/myFetchHelpers.js
+++ helpers/myFetchHelpers.js
@@ -3,10 +3,18 @@ import { dGEBId } from "./myDomHelper";
 /**
  * All CRUD query with asynchronous fetch, you acn have on json,xml or text. Replace format by response.json() or 'response.xml()' or 'response.text()
  * @param {*} url String to url data
- * @param {*} init Object for config post request
+ * @param {*} startingPoint the id of the tag where the HTMLCollection start
  * @returns JSON, XML or Text
  */
- export async function myPost(url, init) {
+
+ export async function myPost(url, startingPoint) {
+  const datas = fdatasCollector(startingPoint),
+    init = {
+      method: 'POST',
+      body: datas,
+      mode: 'cors',
+      cache: 'default'
+    };
   let response = await fetch(url, init);
   if (response.ok === false) {
     throw new Error(`Erreur transmission requête. Statut : ${response.status}`);
@@ -14,7 +22,6 @@ import { dGEBId } from "./myDomHelper";
     const resp = await response.json();
     return Promise.resolve(resp);
   }
-
 };
 
 export async function myGet(url) {
@@ -36,22 +43,14 @@ export async function myDelete(url) {
     return Promise.resolve(resp);
   }
 };
-function createInit (datas) {
-  const fdde = JSON.stringify(datas);
-    const myHeaders = new Headers({
-      "Content-Type": "application/json"
-    });
-    var initFile = {
-      method: 'POST',
-      headers: myHeaders,
-      body: fdde,
-      mode: 'cors',
-      cache: 'default'
-    };
-    return initFile;
-};
 
-function fdatasCollector(start) {
-  const startOn = dGEBId(start);
-  
+function fdatasCollector(startingPoint) {
+  const startOn = dGEBId(startingPoint),
+    fd = new FormData(),
+    datas = startOn.getElementsByClassName('fdata');
+  for (let data of datas) {
+    console.log('id :', data.id , ' - value :', data.value);
+    fd.append(data.id,data.value)
+  }
+  return fd;
 }
