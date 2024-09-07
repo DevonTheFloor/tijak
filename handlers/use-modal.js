57f25a6ff008d792825b39e75b024a7421daee00
DevonTheFloor <devonthefloor@free.fr>
2024-09-07 17:27:47 +0200

    Add only onr modal open in use-modal

--- handlers/use-modal.js
+++ handlers/use-modal.js
@@ -1,3 +1,5 @@
+import { dQSr } from "../helpers/myDomHelper";
+
 /**
  * open custom modal
  * @param {string} idParent - ID of the parent xhere the modal will mounted
@@ -19,4 +21,24 @@ export function closeModal(idParent, queryChidren) {
   const app = document.getElementById(idParent),
 	  box = document.querySelector(queryChidren);
 	app.removeChild(box);
+}
+
+/**
+ * Cose every modals if another is open
+ * @param {Array} modals - Array of modal's tag 
+ * @param {string} idParent - Id of the parent
+ */
+export function onlyOneModalOpen (modals, idParent) {
+	let parent = document.getElementById(idParent);
+	// Vérifier que l'élément parent existe avant de continuer
+	if (!parent) {
+		console.error(`Parent with id "${idParent}" not found`);
+		return;
+	}
+	  modals.forEach(modal => {
+			let child = document.querySelector(modal);
+			if(child) {
+				parent.removeChild(child);
+			}
+		})
 }
